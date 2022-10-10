593b4107cc75aff0af7490ee43bf233148ec8634
cagatay <cagatay.gedik@icloud.com>
2022-10-10 19:29:38 +0300

    Initial Commit

---
+++ iCalories/iCaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCaloriesApp.swift
+//  iCalories
+//
+//  Created by Celil Çağatay Gedik on 10.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct iCaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
