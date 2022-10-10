98ee6224880965e7c84bcfa8b65cd256e8c41769
cagatay <cagatay.gedik@icloud.com>
2022-10-10 21:22:33 +0300

    Time formatting functions

---
+++ iCalories/Helpers/TimeFormatting.swift
@@ -0,0 +1,23 @@
+//
+//  TimeFormatting.swift
+//  iCalories
+//
+//  Created by Celil Çağatay Gedik on 10.10.2022.
+//
+
+import Foundation
+
+
+func calcTimeSince(date: Date) -> String {
+    let minutes = Int(-date.timeIntervalSinceNow)/60
+    let hours = minutes/60
+    let days = hours/24
+    
+    if minutes < 120 {
+        return "\(minutes) minutes ago"
+    } else if minutes >= 120 && hours < 48 {
+        return "\(hours) hours ago"
+    }  else {
+        return "\(days) days ago"
+    }
+}
