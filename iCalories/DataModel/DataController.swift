fae34a512b39b18f31af2f8d1c5204e38b4cec32
cagatay <cagatay.gedik@icloud.com>
2022-10-10 21:21:14 +0300

    DataModel functions

---
+++ iCalories/DataModel/DataController.swift
@@ -0,0 +1,54 @@
+//
+//  DataController.swift
+//  iCalories
+//
+//  Created by Celil Çağatay Gedik on 10.10.2022.
+//
+
+import Foundation
+import CoreData
+
+import Foundation
+import CoreData
+
+class DataController: ObservableObject {
+    // Responsible for preparing a model
+    let container = NSPersistentContainer(name: "FoodModel")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Failed to load data in DataController \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func save(context: NSManagedObjectContext) {
+        do {
+            try context.save()
+            print("Data saved successfully.")
+        } catch {
+            // Handle errors in our database
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+    }
+    
+    func addFood(name: String, calories: Double, context: NSManagedObjectContext) {
+        let food = Food(context: context)
+        food.id = UUID()
+        food.date = Date()
+        food.name = name
+        food.calories = calories
+        
+        save(context: context)
+    }
+    
+    func editFood(food: Food, name: String, calories: Double, context: NSManagedObjectContext) {
+        food.date = Date()
+        food.name = name
+        food.calories = calories
+        
+        save(context: context)
+    }
+}
